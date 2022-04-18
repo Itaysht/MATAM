00f00785aa91486ba7eab83a0c53d9dd8c2d0537
Techkbenj <kbenjamin@campus.technion.ac.il>
2022-04-18 17:43:48 +0300

    Merge branch 'supplied_files' of https://github.com/Itaysht/ex1 into supplied_files

--- RLEList.c
+++ RLEList.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #define NULL_CHAR '\0'
 
-
 struct RLEList_t {
     char letter;
     int numOfReps;
@@ -59,7 +58,6 @@ RLEListResult RLEListAppend(RLEList list, char value)
     newNode->letter = value;
     newNode->numOfReps = 1;
     nextNode->next = newNode;
-    newNode->next = NULL;
     return RLE_LIST_SUCCESS;
 }
 
@@ -71,12 +69,13 @@ int RLEListSize(RLEList list)
     }
 
     int counter = 0;
-    while (list->next)
+    RLEList ptr = list;
+    while (ptr->next)
     {
-        counter += list->numOfReps;
-        list = list->next;
+        counter += ptr->numOfReps;
+        ptr = ptr->next;
     }
-    counter += list->numOfReps;
+    counter += ptr->numOfReps;
     return counter;
 }
 
@@ -86,23 +85,23 @@ RLEListResult RLEListRemove(RLEList list, int index)
     {
         return RLE_LIST_NULL_ARGUMENT;
     }
-    if (index > RLEListSize(list) || index < 1)
+    if (index >= RLEListSize(list) || index < 0)
     {
         return RLE_LIST_INDEX_OUT_OF_BOUNDS;
     }
     int counter = 0;
     RLEList prevNode = list;
     RLEList currNode = list->next;
-    while (counter < index)
+    while (counter <= index)
     {
         counter += currNode->numOfReps;
-        if (counter >= index)
+        if (counter > index)
         {
             break;
         }
         prevNode = currNode;
         currNode = currNode->next;
-        
+
     }
     if (currNode->numOfReps > 1)
     {
@@ -117,12 +116,162 @@ RLEListResult RLEListRemove(RLEList list, int index)
 
 }
 
+char RLEListGet(RLEList list, int index, RLEListResult *result)
+{
+    if (list == NULL)                       //if null is given as an argument
+    {
+        *result = RLE_LIST_NULL_ARGUMENT;
+        return 0;
+    }
+
+    int length = RLEListSize(list);
+    if ((length <= index) || (index < 0))           //if the index is out of bounds
+    {
+        *result = RLE_LIST_INDEX_OUT_OF_BOUNDS;
+        return 0;
+    }
+
+    RLEList ptr = list;
+    ptr = ptr->next;
+    while (ptr != NULL)
+    {
+        if (index < ptr->numOfReps)         //if the index is within the appearences of a specific letter
+        {
+            *result = RLE_LIST_SUCCESS;
+            return ptr->letter;
+        }
+        index = index - ptr->numOfReps;     //else, we'll check the index in the next node
+        ptr = ptr->next;
+    }
+    return 0;             //won't get here anyway
+}
+
+RLEListResult RLEListMap(RLEList list, MapFunction map_function)
+{
+    if ((list == NULL) || (map_function == NULL))    //if one of the arguments are null
+    {
+        return RLE_LIST_NULL_ARGUMENT;
+    }
+
+//    int length = RLEListSize(list);
+//    char* mapped_str = (char*) malloc(length);
+    RLEList ptr = list->next;
+//    int i = 0;
+    while (ptr != NULL)
+    {
+//        for (int j = 0; j < ptr->numOfReps; j++)
+//        {
+//            mapped_str[i+j] = map_function(ptr->letter);
+//        }
+//        i = i + ptr->numOfReps;
+        ptr->letter = map_function(ptr->letter);
+        ptr = ptr->next;
+    }
+    return RLE_LIST_SUCCESS;
+}
+
+/**
+ * NumberOfCharsFromInt: return the number of digit of an int number
+ * @param num - integer number which is the number of appearences in a row of a character
+ * @return
+ *      Number of digit of a integer number
+*/
+static int NumberOfCharsFromInt(int num)
+{
+    int counter = 0;
+    while (num > 0)
+    {
+        counter += 1;
+        num = num / 10;
+    }
+    return counter;
+}
+
+/**
+ *
+ * @param list - The RLE list to retrieve the characters from
+ * @return
+ *      The number of the exported string which includes the char of a node, the number of
+ *      digit of the number of appearences of this char, and the new lines between every node
+ */
+
+static int LengthOfWholeString(RLEList list)
+{
+    if (list == NULL)
+    {
+        return 0;
+    }
+
+    int counter = 0, temp;
+    RLEList ptr = list->next;
+    while (ptr != NULL)
+    {
+        temp = ptr->numOfReps;
+        counter += 2;                          //char and new line
+        counter += NumberOfCharsFromInt(temp); //number of digit
+        ptr = ptr->next;
+    }
+    return counter;
+}
+
+char* RLEListExportToString(RLEList list, RLEListResult* result)
+{
+    if (list == NULL)                           //if the argument list is null
+    {
+        *result = RLE_LIST_NULL_ARGUMENT;
+        return NULL;
+    }
+
+    int length = LengthOfWholeString(list);        //calculating the length of the dynamic array
+    char* str_of_rle = (char*) malloc(length);     //allocating an array of chars which would be the answer
+    if (str_of_rle == NULL)
+    {
+        *result = RLE_LIST_OUT_OF_MEMORY;        //Null if the allocation failed
+        return NULL;
+    }
+    int i = 0, temp, num_of_chars;
+    RLEList ptr = list->next;
+    while (ptr != NULL)                 //each node will split to three parts: letter, chars of int
+    {                                   //according to its length, and new line
+        str_of_rle[i] = ptr->letter;
+        i++;
+        temp = ptr->numOfReps;
+        num_of_chars = NumberOfCharsFromInt(temp);  //calculating the number of digits
+        for (int j = 1; j <= num_of_chars; j++)
+        {
+            str_of_rle[i + num_of_chars - j] = (temp % 10) + '0';   //assigning respectively, '0' converting int to its char symbol
+            temp = temp / 10;
+        }
+        str_of_rle[i+num_of_chars] = '\n';
+        i = i + num_of_chars + 1;                 //moving on to the next node, index-wise
+        ptr = ptr->next;
+    }
+    *result = RLE_LIST_SUCCESS;
+    return str_of_rle;
+}
+
+char PlusOne(char c)
+{
+    return c + 1;
+}
+
 int main()
 {
     //my test
     RLEList head = RLEListCreate();
-    RLEListAppend(head,'c');
+    RLEListAppend(head,'c');                  //test append
     RLEListAppend(head,'f');
+    RLEListAppend(head,'e');
+    RLEListAppend(head,'e');
+    RLEListAppend(head,'e');
+    RLEListAppend(head,'e');
+    RLEListAppend(head,'e');
+    RLEListAppend(head,'e');
+    RLEListAppend(head,'e');
+    RLEListAppend(head,'e');
+    RLEListAppend(head,'e');
+    RLEListAppend(head,'e');
+    RLEListAppend(head,'e');
     RLEListAppend(head,'e');
     RLEListAppend(head,'z');
     RLEListAppend(head,'s');
@@ -130,15 +279,39 @@ int main()
     RLEListAppend(head,'a');
     RLEListAppend(head,'a');
     RLEListAppend(head,'s');
-    for (RLEList ptr = head; ptr != NULL; ptr=ptr->next){
-        printf("%c", ptr->letter);
+    printf("Testing printing:\n");
+    for (RLEList ptr = head; ptr != NULL; ptr=ptr->next){      //test print
+        printf("%c ", ptr->letter);
     }
     printf("\n");
-    RLEListRemove(head, 9);
+    printf("Testing removing single char\n");
+     RLEListRemove(head, 19);                            //test remove
      for (RLEList ptr = head; ptr != NULL; ptr=ptr->next){
-        printf("%c", ptr->letter);
+        printf("%c ", ptr->letter);
     }
-    RLEListDestroy(head);
+    printf("\n");
+    RLEListResult res = RLE_LIST_SUCCESS;
+    printf("Testing Get char from index\n");
+    printf("%c\n", RLEListGet(head, 1, &res));     //test get
+    printf("%c\n", RLEListGet(head, 0, &res));     //test get
+    printf("%c\n", RLEListGet(head, 10, &res));     //test get
+    printf("%c\n", RLEListGet(head, 17, &res));     //test get
+    printf("%c\n", RLEListGet(head, 18, &res));     //test get
+    printf("Testing mapping\n");
+    RLEListMap(head, PlusOne);                                  //test mapping
+    for (RLEList ptr = head; ptr != NULL; ptr=ptr->next){
+        printf("%c ", ptr->letter);
+    }
+    printf("\n");
+    printf("Testing export to string\n");
+    char* ans = RLEListExportToString(head, &res);           //test export to string
+    for (int i = 0; i < LengthOfWholeString(head); i++)
+    {
+        printf("%c", ans[i]);
+    }
+    printf("\n");
+    free(ans);
+    RLEListDestroy(head);                         //test destroy
     return 0;
 }
 
